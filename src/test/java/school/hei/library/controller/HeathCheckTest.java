72abf1fb2debfc5ca1cfb634970a73be743161f6
Randy <raoulrandy3@gmail.com>
2026-06-02 14:59:37 +0300

    initial commit

---
+++ src/test/java/school/hei/library/controller/HeathCheckTest.java
@@ -0,0 +1,13 @@
+package school.hei.library.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeathCheckTest {
+
+    @Test
+    void pong() {
+
+    }
+}
